6630ebc9db120782588053ab6ad39934216f6458
Mehdi <elwafi.courrier99@gmail.com>
2022-03-28 20:35:48 +0000

    Added auth0 jwt support package

--- src/app/controller/services/authentication.service.ts
+++ src/app/controller/services/authentication.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {LocalStorageService} from "./local-storage.service";
+import {Router} from "@angular/router";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(private httpCLient:HttpClient,
+              private storageService:LocalStorageService,
+              private router:Router) { }
+  private url = environment.urlBase+'/users';
+
 }
