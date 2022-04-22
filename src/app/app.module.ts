7b83c48fd1345643a934d614c6e416251aaaf651
Mehdi <elwafi.courrier99@gmail.com>
2022-04-22 15:40:48 +0000

    Added register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DemandeListComponent } from './demande/demande-list/demande-list.compon
 import { DemandeCardComponent } from './demande/demande-card/demande-card.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import {AuthGuard} from "./controller/guards/auth.guard";
+import { RegisterComponent } from './user/register/register.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import {AuthGuard} from "./controller/guards/auth.guard";
     DemandeCreateComponent,
     DemandeListComponent,
     DemandeCardComponent,
-    NavbarComponent
+    NavbarComponent,
+    RegisterComponent
   ],
     imports: [
         BrowserModule,
